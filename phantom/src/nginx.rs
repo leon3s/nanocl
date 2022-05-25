211d1e1287ceee60980d07acf814bdbe7b8b7641
leon3s <lvernisse@student.42.fr>
2022-05-25 17:18:22 +0200

    added containerized services

--- phantom/src/nginx.rs
+++ phantom/src/nginx.rs
@@ -1,54 +1,69 @@
 use std::collections::HashMap;
 
-use bollard::{Docker, container::{CreateContainerOptions, Config, StartContainerOptions}, models::{PortBinding, HostConfig}};
+use bollard::{
+  Docker,
+  container::{
+    CreateContainerOptions,
+    Config
+  },
+  models::{
+    PortBinding,
+    HostConfig
+  }
+};
 
 use crate::docker_helper::*;
 
-async fn create_nginx_container(docker: &Docker) {
-  let options = Some(CreateContainerOptions{
-    name: "nanoclq",
-  });
+fn gen_nginx_host_conf() -> HostConfig {
   let mut port_bindings: HashMap<String, Option<Vec<PortBinding>>> = HashMap::new();
   port_bindings.insert(
-    String::from("5432/tcp"),
+    String::from("80/tcp"),
     Some(vec![PortBinding {
-      host_ip: Some(String::from("")),
-      host_port: Some(String::from("5432")),
+      host_ip: None,
+      host_port: Some(String::from("80")),
     }],
   ));
+  let binds = vec![
+    String::from("/var/lib/nanocl/nginx/sites-enabled:/etc/nginx/sites-enabled"),
+  ];
+  HostConfig {
+    binds: Some(binds),
+    port_bindings: Some(port_bindings),
+    ..Default::default()
+  }
+}
+
+async fn create_nginx_container(docker: &Docker, name: &str) {
+  let image = Some("nginx:latest");
+  let labels = Some(gen_namespace_label("nanocl"));
+  let host_config = Some(gen_nginx_host_conf());
+  let options = Some(CreateContainerOptions{
+    name,
+  });
   let config = Config {
-      image: Some("postgres"),
-      env: Some(vec![
-        "POSTGRES_USER=root",
-        "POSTGRES_PASSWORD=root",
-      ]),
-      host_config: Some(HostConfig {
-        port_bindings: Some(port_bindings),
-        ..Default::default()
-      }),
+      image,
+      labels,
+      host_config,
       ..Default::default()
   };
   let result = match docker.create_container(options, config).await {
-    Ok(result) => result,
     Err(err) => panic!("{:?}", err),
+    Ok(result) => result,
   };
   println!("{:?}", result);
 }
 
-async fn start_nginx_service(docker: &Docker) {
-  docker.start_container(
-    "nanoclq",
-    None::<StartContainerOptions<String>>
-  ).await.unwrap();
-}
-
-pub async fn ctrl_nginx(docker: &Docker) {
+pub async fn ensure_start(docker: &Docker) {
+  let container_name = "nanocl-proxy-nginx";
   install_service(docker, "nginx:latest").await;
   let s_state = get_service_state(docker, "nanocl-ctrl-nginx").await;
 
   if s_state == ServiceState::Uninstalled {
-    create_nginx_container(docker).await;
+    create_nginx_container(docker, container_name).await;
   }
   if s_state != ServiceState::Running {
+    if let Err(err) = start_service(docker, container_name).await {
+      eprintln!("error while starting {} {}", container_name, err);
+    }
   }
 }
