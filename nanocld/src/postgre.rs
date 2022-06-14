c8480affd080074414bce1ef45918243e06ca79f
leon3s <leo.vernisse@gmail.com>
2022-06-14 10:28:21 +0200

    [DOC] postgre.rs

--- nanocld/src/postgre.rs
+++ nanocld/src/postgre.rs
@@ -1,8 +1,14 @@
-use diesel::r2d2::ConnectionManager;
 use diesel::PgConnection;
+use diesel::r2d2::ConnectionManager;
 
 use crate::models::Pool;
 
+/// Create postgres pool
+///
+/// # Examples
+/// ```
+/// let pool = create_pool();
+/// ```
 pub fn create_pool() -> Pool {
   let db_url = "postgres://root:root@nanocl-db-postgre/nanocl";
   let manager = ConnectionManager::<PgConnection>::new(db_url);
