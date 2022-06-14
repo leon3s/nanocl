127a23197dd204897dae51809ffa791ece8e3c17
leon3s <leo.vernisse@gmail.com>
2022-06-14 10:25:04 +0200

    [DOC] repositories/cluster

--- nanocld/src/repositories/cluster.rs
+++ nanocld/src/repositories/cluster.rs
@@ -1,3 +1,4 @@
+//! Functions to manipulate clusters in database
 use ntex::web;
 use uuid::Uuid;
 use diesel::prelude::*;
@@ -5,128 +6,206 @@ use diesel::prelude::*;
 use crate::utils::get_pool_conn;
 use crate::controllers::errors::HttpError;
 use crate::repositories::errors::db_blocking_error;
-use crate::models::{
-    Pool,
-    ClusterItem,
-    ClusterCreate,
-    PgDeleteGeneric,
-};
+use crate::models::{Pool, ClusterItem, ClusterCreate, PgDeleteGeneric};
 
+/// Return a fresh cluster with id and gen_id
+///
+/// # Arguments
+///
+/// * `nsp` - The namespace to add cluster in
+/// * `item` - Cluster to create
+/// * `pool` - Posgresql database pool
+///
+/// # Examples
+///
+/// ```
+/// // Create a simple cluster
+///
+/// use crate::repositories::cluster;
+/// let nsp = String::from("default");
+/// let new_cluster = ClusterCreate {
+///  name: String::from("test-cluster")
+/// }
+/// cluster::create_for_namespace(nsp, new_cluster, &pool).await;
+/// ```
 pub async fn create_for_namespace(
-    nsp: String,
-    item: ClusterCreate,
-    pool: &web::types::State<Pool>,
+  nsp: String,
+  item: ClusterCreate,
+  pool: &web::types::State<Pool>,
 ) -> Result<ClusterItem, HttpError> {
-    use crate::schema::clusters::dsl::*;
-    let conn = get_pool_conn(pool)?;
+  use crate::schema::clusters::dsl::*;
+  let conn = get_pool_conn(pool)?;
 
-    let res = web::block(move || {
-        let genid = nsp.to_owned() + "-" + &item.name;
-        let new_cluster = ClusterItem {
-            id: Uuid::new_v4(),
-            name: item.name,
-            gen_id: genid,
-            namespace: nsp,
-        };
+  let res = web::block(move || {
+    let genid = nsp.to_owned() + "-" + &item.name;
+    let new_cluster = ClusterItem {
+      id: Uuid::new_v4(),
+      name: item.name,
+      gen_id: genid,
+      namespace: nsp,
+    };
 
-        diesel::insert_into(clusters)
-            .values(&new_cluster)
-            .execute(&conn)?;
-        Ok(new_cluster)
-    }).await;
+    diesel::insert_into(clusters)
+      .values(&new_cluster)
+      .execute(&conn)?;
+    Ok(new_cluster)
+  })
+  .await;
 
-    match res {
-        Err(err) => Err(db_blocking_error(err)),
-        Ok(item) => Ok(item),
-    }
+  match res {
+    Err(err) => Err(db_blocking_error(err)),
+    Ok(item) => Ok(item),
+  }
 }
 
+/// Return found cluster or an error otherwise
+///
+/// # Arguments
+///
+/// * `id` - Id of the cluster
+/// * `pool` - Posgresql database pool
+///
+/// # Examples
+///
+/// ```
+/// // Find cluster by id
+///
+/// use crate::repositories::cluster;
+/// cluster::find_by_id(id, &pool).await;
+/// ```
 pub async fn find_by_id(
-    id: Uuid,
-    pool: &web::types::State<Pool>,
+  id: Uuid,
+  pool: &web::types::State<Pool>,
 ) -> Result<ClusterItem, HttpError> {
-    use crate::schema::clusters::dsl;
+  use crate::schema::clusters::dsl;
 
-    let conn = get_pool_conn(pool)?;
-    let res = web::block(move || {
-        dsl::clusters.filter(dsl::id.eq(id)).get_result(&conn)
-    }).await;
+  let conn = get_pool_conn(pool)?;
+  let res =
+    web::block(move || dsl::clusters.filter(dsl::id.eq(id)).get_result(&conn))
+      .await;
 
-    match res {
-        Err(err) => Err(db_blocking_error(err)),
-        Ok(item) => Ok(item),
-    }
+  match res {
+    Err(err) => Err(db_blocking_error(err)),
+    Ok(item) => Ok(item),
+  }
 }
 
+/// Return found cluster or an error otherwise
+///
+/// # Arguments
+///
+/// * `gen_id` - Generated id of the cluster
+/// * `pool` - Posgresql database pool
+///
+/// # Examples
+///
+/// ```
+/// // Find cluster by id
+///
+/// use crate::repositories::cluster;
+/// cluster::find_by_gen_id(gen_id, &pool).await;
+/// ```
 pub async fn find_by_gen_id(
-    gen_id: String,
-    pool: &web::types::State<Pool>,
+  gen_id: String,
+  pool: &web::types::State<Pool>,
 ) -> Result<ClusterItem, HttpError> {
-    use crate::schema::clusters::dsl;
+  use crate::schema::clusters::dsl;
 
-    let conn = get_pool_conn(pool)?;
-    let res = web::block(move || {
-        dsl::clusters.filter(dsl::gen_id.eq(gen_id)).get_result(&conn)
-    }).await;
+  let conn = get_pool_conn(pool)?;
+  let res = web::block(move || {
+    dsl::clusters
+      .filter(dsl::gen_id.eq(gen_id))
+      .get_result(&conn)
+  })
+  .await;
 
-    match res {
-        Err(err) => Err(db_blocking_error(err)),
-        Ok(item) => Ok(item),
-    }
+  match res {
+    Err(err) => Err(db_blocking_error(err)),
+    Ok(item) => Ok(item),
+  }
 }
 
+/// Return list of cluster of given namespace
+///
+/// # Arguments
+///
+/// * `nsp` - Namespace name
+/// * `pool` - Posgresql database pool
+///
+/// # Examples
+///
+/// ```
+/// // List cluster by namespace
+///
+/// use crate::repositories::cluster;
+/// cluster::find_by_namespace(gen_id, &pool).await;
+/// ```
 pub async fn find_by_namespace(
-    nsp: String,
-    pool: &web::types::State<Pool>,
+  nsp: String,
+  pool: &web::types::State<Pool>,
 ) -> Result<Vec<ClusterItem>, HttpError> {
-    use crate::schema::clusters::dsl::*;
+  use crate::schema::clusters::dsl::*;
 
-    let conn = get_pool_conn(pool)?;
-    let res = web::block(move || {
-        clusters.filter(namespace.eq(nsp))
-        .load(&conn)
-    }).await;
-    match res {
-        Err(err) => Err(db_blocking_error(err)),
-        Ok(items) => Ok(items),
-    }
+  let conn = get_pool_conn(pool)?;
+  let res =
+    web::block(move || clusters.filter(namespace.eq(nsp)).load(&conn)).await;
+  match res {
+    Err(err) => Err(db_blocking_error(err)),
+    Ok(items) => Ok(items),
+  }
 }
 
-pub async fn delete_for_gen_id(
-    gen_id: String,
-    pool: &web::types::State<Pool>,
+/// Return number of deleted entries
+///
+/// # Arguments
+///
+/// * `gen_id` - Generated id of the cluster to delete
+/// * `pool` - Posgresql database pool
+///
+/// # Examples
+///
+/// ```
+/// // Delete cluster by generated id
+///
+/// use crate::repositories::cluster;
+/// cluster::delete_by_gen_id(gen_id, &pool).await;
+/// ```
+pub async fn delete_by_gen_id(
+  gen_id: String,
+  pool: &web::types::State<Pool>,
 ) -> Result<PgDeleteGeneric, HttpError> {
-    use crate::schema::clusters::dsl;
+  use crate::schema::clusters::dsl;
 
-    let conn = get_pool_conn(pool)?;
-    let res = web::block(move || {
-        diesel::delete(dsl::clusters)
-        .filter(dsl::gen_id.eq(gen_id))
-        .execute(&conn)
-    }).await;
+  let conn = get_pool_conn(pool)?;
+  let res = web::block(move || {
+    diesel::delete(dsl::clusters)
+      .filter(dsl::gen_id.eq(gen_id))
+      .execute(&conn)
+  })
+  .await;
 
-    match res {
-        Err(err) => Err(db_blocking_error(err)),
-        Ok(result) => Ok(PgDeleteGeneric {
-            count: result,
-        })
-    }
+  match res {
+    Err(err) => Err(db_blocking_error(err)),
+    Ok(result) => Ok(PgDeleteGeneric { count: result }),
+  }
 }
 
 #[cfg(test)]
 mod test_cluster {
-    use ntex::web;
+  use ntex::web;
 
-    use crate::postgre;
+  use crate::postgre;
 
-    use super::*;
+  use super::*;
 
-    #[ntex::test]
-    async fn main() {
-        let pool = postgre::create_pool();
-        let pool_state = web::types::State::new(pool);
+  #[ntex::test]
+  async fn main() {
+    let pool = postgre::create_pool();
+    let pool_state = web::types::State::new(pool);
 
-        let res = find_by_namespace(String::from("default"), &pool_state).await.unwrap();
-        assert!(res.is_empty());
-    }
+    let res = find_by_namespace(String::from("default"), &pool_state)
+      .await
+      .unwrap();
+    assert!(res.is_empty());
+  }
 }
