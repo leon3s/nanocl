171bc4568ccfee33a3c80fc1935658424bbc8ebb
leon3s <leo.vernisse@gmail.com>
2022-06-14 10:27:48 +0200

    [LINT] repositories/mod.rs

--- nanocld/src/repositories/mod.rs
+++ nanocld/src/repositories/mod.rs
@@ -1,5 +1,6 @@
-pub mod cluster;
+//! Functions to manipulate model in database
 pub mod errors;
+pub mod cluster;
+pub mod namespace;
 pub mod git_repository;
 pub mod git_repository_branch;
-pub mod namespace;
