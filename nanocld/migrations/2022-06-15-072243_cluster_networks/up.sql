69a83574e9ad113f3cffe5965b1083c483639034
leon3s <leo.vernisse@gmail.com>
2022-06-16 13:48:30 +0200

    [REFACTO] added docker_network_id in cluster_network table

--- nanocld/migrations/2022-06-15-072243_cluster_networks/up.sql
+++ nanocld/migrations/2022-06-15-072243_cluster_networks/up.sql
@@ -2,5 +2,6 @@
 CREATE TABLE "cluster_networks" (
   "id" UUID NOT NULL UNIQUE PRIMARY KEY,
   "name" VARCHAR(100) NOT NULL UNIQUE,
+  "docker_network_id" VARCHAR NOT NULL UNIQUE,
   "cluster_id" UUID NOT NULL
 );
