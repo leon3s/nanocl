6dfb4b90128225b62153575493e965de6c27b303
leon3s <leo.vernisse@gmail.com>
2022-06-16 18:54:22 +0200

    [DOC] Added commentary on controllers/mod.rs

--- nanocld/src/controllers/mod.rs
+++ nanocld/src/controllers/mod.rs
@@ -1,6 +1,11 @@
 //! File to export controller
 pub mod errors;
+/// Manage cluster
 pub mod cluster;
+/// Manage namespace
 pub mod namespace;
+
+/// Manage git repository
 pub mod git_repository;
+/// Manage cluster network
 pub mod cluster_network;
