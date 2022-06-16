437adfb29fa923ed62e0dde20692bb76a4006d7b
leon3s <leo.vernisse@gmail.com>
2022-06-16 15:17:58 +0200

    [DOC] Added doc commentary in nanocld/services/utils.rs

--- nanocld/src/services/utils.rs
+++ nanocld/src/services/utils.rs
@@ -4,9 +4,7 @@ use bollard::{
   Docker,
   errors::Error as DockerError,
   image::{CreateImageOptions, BuildImageOptions},
-  network::{
-    ConnectNetworkOptions, CreateNetworkOptions, InspectNetworkOptions,
-  },
+  network::{CreateNetworkOptions, InspectNetworkOptions},
   container::StartContainerOptions,
 };
 
@@ -23,13 +21,42 @@ pub enum NetworkState {
   Ready,
 }
 
-/// # Generate label namespace
-pub fn gen_label_namespace(namespace: &str) -> HashMap<&str, &str> {
+/// # Generate labels width a namespace
+///
+/// # Arguments
+/// - [namespace](str) the name of the namespace
+///
+/// # Return
+/// [labels](HashMap) a hashmap of strings with namespace key as given value
+///
+/// # Examples
+/// ```rust,norun
+/// use crate::services;
+///
+/// services::utils::gen_labels_with_namespace("default");
+/// ```
+pub fn gen_labels_with_namespace(namespace: &str) -> HashMap<&str, &str> {
   let mut labels: HashMap<&str, &str> = HashMap::new();
   labels.insert("namespace", namespace);
   labels
 }
 
+/// # Start a service
+/// Start service by it's name
+///
+/// # Arguments
+/// - [docker](Docker) bollard docker instance
+/// - [name](str) name of the service to start
+///
+/// # Return
+/// if sucess return nothing a [docker error](DockerError) is returned if an error occur
+///
+/// # Examples
+/// ```rust,norun
+/// use crate::services;
+///
+/// services::utils::start_service(&docker, "nanocl-proxy-nginx").await;
+/// ```
 pub async fn start_service(
   docker: &Docker,
   name: &str,
@@ -40,6 +67,22 @@ pub async fn start_service(
   Ok(())
 }
 
+/// # Build a service
+/// Build a nxthat service from github
+///
+/// # Arguments
+/// - [docker](Docker) bollard docker instance
+/// - [name](str) name of the service to build
+///
+/// # Return
+/// if sucess return nothing a [docker error](DockerError) is returned if an error occur
+///
+/// /// # Examples
+/// ```rust,norun
+/// use crate::services;
+///
+/// services::utils::build_service(&docker, "nanocl-proxy-nginx").await;
+/// ```
 pub async fn build_service(
   docker: &Docker,
   service_name: &'static str,
@@ -61,6 +104,22 @@ pub async fn build_service(
   Ok(())
 }
 
+/// # Install a service
+/// Install a service from docker image
+///
+/// # Arguments
+/// - [docker](Docker) bollard docker instance
+/// - [name](str) name of the service to install
+///
+/// # Return
+/// if sucess return nothing a [docker error](DockerError) is returned if an error occur
+///
+/// /// # Examples
+/// ```rust,norun
+/// use crate::services;
+///
+/// services::utils::install_service(&docker, "postgresql").await;
+/// ```
 pub async fn install_service(
   docker: &Docker,
   image_name: &'static str,
@@ -81,19 +140,23 @@ pub async fn install_service(
   Ok(())
 }
 
-pub async fn _connect_to_network(
-  docker: &Docker,
-  container_name: &str,
-  network_name: &str,
-) -> Result<(), DockerError> {
-  let config = ConnectNetworkOptions {
-    container: container_name,
-    ..Default::default()
-  };
-  docker.connect_network(network_name, config).await?;
-  Ok(())
-}
-
+/// # Install a service
+/// Install a service from docker image
+///
+/// # Arguments
+/// - [docker](Docker) bollard docker instance
+/// - [name](str) name of the service to install
+///
+/// # Return
+/// /// if success return [network state](NetworkState)
+/// a [docker error](DockerError) is returned if an error occur
+///
+/// /// # Examples
+/// ```rust,norun
+/// use crate::services;
+///
+/// services::utils::get_network_state(&docker, "network-name").await;
+/// ```
 pub async fn get_network_state(
   docker: &Docker,
   network_name: &str,
@@ -124,6 +187,22 @@ pub async fn get_network_state(
   Ok(NetworkState::Ready)
 }
 
+/// # Create a network
+/// Create a network by name with default settings using docker api
+///
+/// # Arguments
+/// - [docker](Docker) bollard docker instance
+/// - [name](str) name of the network to create
+///
+/// # Return
+/// if sucess return nothing a [docker error](DockerError) is returned if an error occur
+///
+/// /// # Examples
+/// ```rust,norun
+/// use crate::services;
+///
+/// services::utils::create_network(&docker, "network-name").await;
+/// ```
 pub async fn create_network(
   docker: &Docker,
   network_name: &str,
@@ -136,6 +215,23 @@ pub async fn create_network(
   Ok(())
 }
 
+/// # Get service state
+/// Get state of a service by his name
+///
+/// # Arguments
+/// - [docker](Docker) bollard docker instance
+/// - [name](str) name of the service
+///
+/// # Return
+/// if success return [service state](ServiceState)
+/// a [docker error](DockerError) is returned if an error occur
+///
+/// # Examples
+/// ```rust,norun
+/// use crate::services;
+///
+/// services::utils::get_service_state(&docker, "nanocl-proxy-nginx").await;
+/// ```
 pub async fn get_service_state(
   docker: &Docker,
   container_name: &'static str,
