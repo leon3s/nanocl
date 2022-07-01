80f3b3891944ae365bdd86a89447713cf7f23199
leon3s <leo.vernisse@gmail.com>
2022-07-01 14:31:07 +0200

    [REFACTO] fake_path and install scripts

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-## name: install_dev.sh
+## name: install.sh
 set -e -x
 
 sudo groupapp nanocl
-sudo mkdir -p /var/lib/nanocl
+sudo cp -r ./var/lib/nanocl /var/lib/nanocl
 sudo mkdir -p /var/run/nanocl
