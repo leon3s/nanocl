37b90b1be03ecd00e038e2ebf12394074d128a0b
leon3s <leo.vernisse@gmail.com>
2022-07-01 18:01:37 +0200

    [BUGFIX] nanocld: fix migration script

--- nanocld/src/postgre.rs
+++ nanocld/src/postgre.rs
@@ -3,6 +3,15 @@ use diesel::r2d2::ConnectionManager;
 
 use crate::models::Pool;
 
+pub fn create_migration_pool(host: String) -> Pool {
+  let db_url = "postgres://root:root@".to_owned() + &host;
+  let manager = ConnectionManager::<PgConnection>::new(db_url);
+
+  r2d2::Pool::builder()
+    .build(manager)
+    .expect("Failed to create pool.")
+}
+
 /// # Create pool
 /// Create an pool connection to postgres database
 ///
